3e0adc71377571cda2dbef2e5ecdb1e296ddba0c
ajkn1904 <ajknishat@gmail.com>
2022-11-08 02:38:18 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+require('dotenv').config()
+
 
 app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7splzic.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Home Service API running!');
