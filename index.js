ef6c4e5677dde67902beebe9efff5ff833f1eb60
ajkn1904 <ajknishat@gmail.com>
2022-11-08 14:47:47 +0600

    mongodb read data operation done

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-const express = require('express')
+const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const cors = require('cors')
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
 
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7splzic.mongodb.net/?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+
+    try{
+        const serviceCollections = client.db('homeService').collection('services')
+
+        app.get('/services', async(req,res) => {
+          const query = {};
+          const cursor = serviceCollections.find(query);
+          const services = await cursor.toArray();
+          res.send(services);
+        });
+    }
+    finally{
+
+    }
+    
+}
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
     res.send('Home Service API running!');
