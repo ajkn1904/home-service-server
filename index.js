e093f4d147f8e8dd0c4738949b2cfb3ee1c5e6cb
ajkn1904 <ajknishat@gmail.com>
2022-11-10 23:54:40 +0600

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken')
@@ -6,10 +7,14 @@ const jwt = require('jsonwebtoken')
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-require('dotenv').config();
 
 
-app.use(cors());
+
+app.use(cors(
+  {
+    origin : '*'
+  }
+  ));
 app.use(express.json());
 
 
@@ -110,6 +115,14 @@ async function reviewApi(){
         res.send(review);
       })
 
+      app.get('/specificReview/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { 
+          _id: ObjectId(id) };
+        const spreview = await serviceCollections.findOne(query);
+        res.send(spreview);
+      })
+
       
       app.get('/myreviews', verifyToken, async (req, res) => {
         const decoded = req.decoded
@@ -139,16 +152,19 @@ async function reviewApi(){
       })
 
 
-      app.patch('updateReview/:id', verifyToken, async(req, res) => {
+      app.put('/updateReview/:id', verifyToken, async(req, res) => {
         const id = req.params.id;
         const status = req.body.status;
         const query = { _id: ObjectId(id)}
+        
+        const option = {upsert: true};
         const newReview = {
           $set:{
-            status: status
+            text: status
           }
         }
-        const result = await serviceCollections.updateOne(query, newReview)
+        console.log(newReview)
+        const result = await serviceCollections.updateOne(query, newReview, option)
         res.send(result)
       })
 
